05ebd629fa1d922c311f8aa0b5fd65f61fdc6c5a
rafarastrero.46 <rafarastrero.46@gmail.com>
2014-08-21 14:57:07 +0000

    Se ha sobreescrito el metodo create de editor y de maquetador para que se inserte automaticamente en la tabla de "res_groups_users_rel" ya demás se le envíe un correo con los datos de acceso

--- MoleQla/Models/editor.py
+++ MoleQla/Models/editor.py
@@ -8,11 +8,38 @@ class editor(osv.osv):
     
     _columns = {
         'nombre' : fields.char('Nombre', size=128),
-        'apellidos': fields.char('Apellidos', size=128), 
-        'descripcion': fields.text('Descripción'),       
-        'user_id': fields.many2one('res.users','Usuario'),
-        'seccion_id': fields.many2one('seccion','Sección'),
+        'apellidos': fields.char('Apellidos', size=128),
+        'descripcion': fields.text('Descripción'),
+        'user_id': fields.many2one('res.users', 'Usuario'),
+        'seccion_id': fields.many2one('seccion', 'Sección'),
         }
+    
+    def create(self, cr, uid, vals, context=None):
+        id_res_users = vals['user_id']
+                
+        # Cogemos la id del grupo Editor Seccion
+        cr.execute("SELECT id FROM res_groups WHERE name = 'Editor Seccion'")
+        id_gr_editor = cr.fetchone()
+        
+        # Insertamos una contraseña por defecto
+        cr.execute("UPDATE res_users SET password='password' WHERE id = " + str(id_res_users))
+        
+        # Insertamos al usuario en el grupo de editores
+        cr.execute("INSERT INTO res_groups_users_rel(gid, uid) VALUES (" + str(id_gr_editor[0]) + "," + str(id_res_users) + ")")
+        
+        # Enviamos un correo al editor registrado con sus datos
+        # Asunto y texto del email
+        user_obj = self.pool.get('res.users')
+        editor_id = user_obj.search(cr, uid, [('id', '=', id_res_users)])
+        editor_regis = user_obj.browse(cr, uid, editor_id, context)
+        email = editor_regis[0].login
+        asunto = "Welcome to MoleQla "
+        texto = "Your login details are: <br /> <ul> <li>User: "+email+"</li> <li>Password: password</li> </ul> <br /> <b>Please choose a password to access</b>"
+        
+        # Se envia el correo
+        correo_obj = self.pool.get('correo')        
+        correo_obj.mail(cr, 1, email, asunto, texto) 
+        return super(editor, self).create(cr, uid, vals, context)     
     
     def name_get(self, cr, uid, ids, context=None):
         
--- MoleQla/Models/maquetador.py
+++ MoleQla/Models/maquetador.py
@@ -14,6 +14,33 @@ class maquetador(osv.osv):
         'seccion_id': fields.many2one('seccion','Sección'),
         }
 
+    def create(self, cr, uid, vals, context=None):
+        id_res_users = vals['user_id']
+                
+        # Cogemos la id del grupo Editor Seccion
+        cr.execute("SELECT id FROM res_groups WHERE name = 'Maquetador'")
+        id_gr_maquetador = cr.fetchone()
+        
+        # Insertamos una contraseña por defecto
+        cr.execute("UPDATE res_users SET password='password' WHERE id = " + str(id_res_users))
+        
+        # Insertamos al usuario en el grupo de editores
+        cr.execute("INSERT INTO res_groups_users_rel(gid, uid) VALUES (" + str(id_gr_maquetador[0]) + "," + str(id_res_users) + ")")
+        
+        # Enviamos un correo al editor registrado con sus datos
+        # Asunto y texto del email
+        user_obj = self.pool.get('res.users')
+        maquetador_id = user_obj.search(cr, uid, [('id', '=', id_res_users)])
+        maquetador_regis = user_obj.browse(cr, uid, maquetador_id, context)
+        email = maquetador_regis[0].login
+        asunto = "Welcome to MoleQla "
+        texto = "Your login details are: <br /> <ul> <li>User: "+email+"</li> <li>Password: password</li> </ul> <br /> <b>Please choose a password to access</b>"
+        
+        # Se envia el correo
+        correo_obj = self.pool.get('correo')        
+        correo_obj.mail(cr, 1, email, asunto, texto) 
+        return super(maquetador, self).create(cr, uid, vals, context)  
+
     def name_get(self, cr, uid, ids, context=None):
         
         if context is None:
